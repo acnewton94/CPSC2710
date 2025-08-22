6a689f297f1a1ef3e73fba2ce036a1d70b899056
Alex Newton <azn0100@auburn.edu>
2025-08-22 19:49:57 -0400

    Add getters, setters, and toString()

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -5,4 +5,29 @@ public class SeatReservation {
     private LocalDate flightDate;
     private String firstName;
     private String lastName;
+
+    public String getFlightDesignator() { return flightDesignator; }
+    public void setFlightDesignator(String fd) { this.flightDesignator = fd; }
+
+    public LocalDate getFlightDate() { return flightDate; }
+    public void setFlightDate(LocalDate date) { this.flightDate = date; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String fn) { this.firstName = fn; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String ln) { this.lastName = ln; }
+
+    @Override
+    public String toString() {
+        String fd  = (flightDesignator == null || flightDesignator.isEmpty()) ? "null" : flightDesignator;
+        String dt  = (flightDate == null) ? "null" : flightDate.toString();
+        String fn  = (firstName == null || firstName.isEmpty()) ? "null" : firstName;
+        String ln  = (lastName == null || lastName.isEmpty()) ? "null" : lastName;
+
+        return "SeatReservation{flightDesignator=" + fd
+             + ",flightDate=" + dt
+             + ",firstName=" + fn
+             + ",lastName=" + ln + "}";
+    }
 }
