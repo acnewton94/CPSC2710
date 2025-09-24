22b223f74440c71b983f909a77422a19c748ba39
Alex Newton <azn0100@auburn.edu>
2025-09-24 11:51:01 -0400

    Module5: Part1 CSS + Part2 launcher, assets, and wiring

--- module5/src/main/java/edu/au/cpsc/miscstyle/Part1Application.java
+++ module5/src/main/java/edu/au/cpsc/miscstyle/Part1Application.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.net.URL;
 
 /*
  Project: Module 5
@@ -11,14 +12,27 @@ import javafx.stage.Stage;
  Auburn Email: azn0100@auburn.edu
  Date: 2025-09-22
 */
-
 public class Part1Application extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxml = new FXMLLoader(
-                Part1Application.class.getResource("/edu/au/cpsc/miscstyle/part1.fxml")
-        );
-        Scene scene = new Scene(fxml.load());
+        URL fxml = Part1Application.class.getResource("/edu/au/cpsc/miscstyle/part1.fxml");
+        if (fxml == null) {
+            throw new IllegalStateException("Cannot find /edu/au/cpsc/miscstyle/part1.fxml on classpath");
+        }
+
+        FXMLLoader loader = new FXMLLoader(fxml);
+        Scene scene = new Scene(loader.load());
+
+        URL cssUrl = Part1Application.class.getResource("/edu/au/cpsc/miscstyle/style/main.css");
+        if (cssUrl == null) {
+            throw new IllegalStateException("Cannot find /edu/au/cpsc/miscstyle/style/main.css on classpath");
+        }
+        String css = cssUrl.toExternalForm();
+        if (!scene.getStylesheets().contains(css)) {
+            scene.getStylesheets().add(css);
+        }
+
         stage.setTitle("Alex Newton's Flight Designator App");
         stage.setScene(scene);
         stage.show();
