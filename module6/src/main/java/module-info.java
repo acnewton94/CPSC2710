19749e1f9b14d3cddcc8ae69e4cdb5d0946bb80a
Alex Newton <azn0100@auburn.edu>
2025-09-28 18:26:17 -0400

    module6: add pom.xml and module-info

---
+++ module6/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module module6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens edu.au.cpsc.part1 to javafx.fxml;
+    opens edu.au.cpsc.module6 to javafx.fxml;
+
+    exports edu.au.cpsc.part1;
+    exports edu.au.cpsc.module6;
+}
