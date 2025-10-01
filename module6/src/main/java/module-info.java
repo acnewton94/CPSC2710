4dfa8ddc12145913c31b0d037abf9edcafacb053
Alex Newton <azn0100@auburn.edu>
2025-09-30 23:03:17 -0400

    Part1 complete: bindings implemented; resources & module-info set

--- module6/src/main/java/module-info.java
+++ module6/src/main/java/module-info.java
@@ -1,11 +1,7 @@
 module module6 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.base;
 
     opens edu.au.cpsc.part1 to javafx.fxml;
-    opens edu.au.cpsc.module6 to javafx.fxml;
-
     exports edu.au.cpsc.part1;
-    exports edu.au.cpsc.module6;
 }
